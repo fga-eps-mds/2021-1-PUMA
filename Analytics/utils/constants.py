1612ed8eb100bea5c1f3fdfc11d944494f40f363
Gabriel Tiveron <gabrielmtiveron@gmail.com>
2021-10-31 15:18:24 -0300

    [#108] Adding base files

---
+++ Analytics/utils/constants.py
@@ -0,0 +1,25 @@
+SERVICES = [
+    'UserService',
+    'ProjectService',
+    'NotifyService',
+    'AlocateService',
+    'ApiGateway',
+    'Frontend'
+]
+
+METRICS = [
+    'files',
+    'functions',
+    'complexity',
+    'coverage',
+    'ncloc',
+    'comment_lines_density',
+    'duplicated_lines_density',
+    'security_rating',
+    'tests',
+    'test_success_density',
+    'test_execution_time',
+    'reliability_rating'
+]
+
+
