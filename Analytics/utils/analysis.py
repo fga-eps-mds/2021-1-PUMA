ece472af655a1cbf5ed7b8a2b43c52860e812d67
Gabriel Tiveron <gabrielmtiveron@gmail.com>
2021-11-08 20:49:19 -0300

    [#108] Adding code quality analysis

--- Analytics/utils/analysis.py
+++ Analytics/utils/analysis.py
@@ -11,21 +11,22 @@ a gestão do time, que desempenhou menos que o esperado, necessitando, então, a
 '''
 ## Api Gateway
 
-No Gateway, por ser um serviço de intermediação de requisição foram feitos apenas testes de integração, e não foi possível gerar a coleta desses dados para
-a análise.
+No Gateway, por ser um serviço de intermediação de requisição foram feitos apenas testes de integração, e não foi possível gerar a coleta
+desses dados para análise. Porém é possível conferir e executar a suite de testes para esse serviço.
 ''',
     'UserService':
 '''
 ## User Service
 
-* Complexidade
-    - A complexidade manteve uma constância durante todas as releases geradas e ao final, com uma refatoração, pode-se perceber uma melhora.
-* Sucesso nos testes
-    - Nas primeiras releases não foram desenvolvidos testes devido a diversos fatores, desde trancamento de integrantes do grupo até um desempenho abaixo do esperado nas entregas de histórias de usuário. Dessa forma, o gráfico se manteve constante, até a última release, onde foi possível realizar essas ''',
+Durante a execução do projeto, devido a diversos fatores, foi necessária a priorização da entrega de
+Histórias de Usuários. Dessa forma, é possível observar que a qualidade de código e, principalmente, os testes
+tiveram esforços alocados apenas na última release, onde foram feitos testes e pequenas refatorações.
+''',
     'Frontend':\
 '''
 ## Frontend
 
-No forntend não foi possível a geração de testes.
+No Frontend não foi possível a geração de testes devido a demanda do time em outras atividades e na confecção de testes para os outros
+serviços.
 '''
 }
